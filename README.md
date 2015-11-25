ce448a519e06d56754085690aa103438413f03ab
Dan Cardin <dcardin@insightsquared.com>
2015-11-25 21:58:27 +0000

    Add docstrings to the public methods

---
+++ README.md
@@ -0,0 +1,21 @@
+SQL Query Builder
+---
+
+This modules defines a class which allow the building of SQL queries in a functional style.
+
+This class lazily builds SQL queries. Each method called on the class returns another `Query` object which will only get evaluated once the `build` method is called.
+
+Example:
+
+    q = Query('Person').select('id', 'age')
+    filtered = q.filter(name='Bill')
+    name_ordered = (filtered
+        .select('name')
+        .order_by('name')
+        .build()
+    )
+    age_ordered = filtered.order_by('age').build()
+
+    name_ordered == 'SELECT id, age, name FROM Person where name='Bill' ORDER BY name;'
+
+    age_ordered == 'SELECT id, age FROM Person where name='Bill' ORDER BY age;'
--- query.py
+++ query.py
@@ -1,17 +1,103 @@
+"""SQL Query building class.
+
+This modules defines a class which allow the building
+of SQL queries in a functional style.
+"""
 from itertools import groupby
 
 
-class StaticQuery(object):
-    @staticmethod
-    def _order(key):
-        return {
-            Query._select: 0,
-            Query._table: 1,
-            Query._filter: 2,
-        }[key.func]
+class Query(object):
+    def __init__(self, table=None, **_kwargs):
+        """A class for generating SQL queries.
+
+        This class lazily builds SQL queries. Each method called
+        on the class returns another `Query` object which will only
+        get evaluated once the `build` method is called.
+
+        Example:
+            >>> print(Query('Person')
+            >>>    .select('id', 'age')
+            >>>    .filter(name='Bill')
+            >>>    .order_by('name')
+            >>>    .build()
+            >>> )
+            SELECT id, age FROM Person where name='Bill' ORDER BY name;
+
+        Args:
+            table (str): The name of the table to which the query will point
+        """
+        if table:
+            self.func = Query._table
+
+        self._backref = _kwargs.get('_backref', None)
+        self._func = _kwargs.get('_func', None)
+        self._args = _kwargs.get('_args', None)
+        self._kwargs = _kwargs.get('_kwargs', None)
+
+    def select(self, *args):
+        """Set the columns that the query selects.
+
+        Makes the query generated be a SELECT type command like:
+            SELECT * FROM Table;
+
+        *args (str): A variable sized list of column names to select
+        """
+        return Query(backref=self, func=Query._select, args=args)
+
+    def filter(self, **kwargs):
+        """Filter the results of the query.
+
+        Limits the results by turning kwargs into `WHERE` conditions. This
+        turns `.filter(name='value')` into `WHERE column_name='value'`.
+
+        Args:
+            kwargs: Arbitrary keyword args which maps {column_name: 'value'}
+        """
+        return Query(backref=self, func=Query._filter, kwargs=kwargs)
+
+    def order_by(self, *args):
+        """Order the results.
+
+        Adds an `ORDER BY` clause to the query that orders the results.
+
+        Args:
+            args: A variable list of arguments to order the results by.
+        """
+        return Query(backref=self, func=Query._order_by, args=args)
+
+    def build(self):
+        """Evaluate the query.
+
+        Returns:
+            str: the evaluated query string.
+        """
+        backrefs = sorted(Query._backrefs(self), key=lambda b: id(b.func))
+        groups = groupby(backrefs, lambda k: k.func)
+
+        kwargs = {}
+        for key, section in groups:
+            kwargs[key.__name__[1:]] = getattr(Query, key.__name__)(section)
+
+        result = '{select} {table}'
+        if kwargs.get('filter'):
+            result += ' {filter}'
+        if kwargs.get('order_by'):
+            result += ' {order_by}'
+        result += ';'
+
+        return result.format(**kwargs)
 
     @staticmethod
     def _backrefs(back):
+        """Traverse through the backref links recursively.
+
+        Recurses backwards on the `back` instance, yielding
+        the `back` instance until `back._backref` is None.
+
+        Returns:
+            A generator object to iterate over the references to the
+            composed previous instances of the `Query`.
+        """
         while True:
             if back is None:
                 break
@@ -19,25 +105,50 @@ class StaticQuery(object):
             back = back.backref
 
     @staticmethod
+    def _args_get(refs):
+        """Aggregate the iterable `refs.args` into a single iterable.
+
+        Args:
+            refs (Iterable): A list of lists of args which get aggregated
+                             into a single list.
+
+        Returns:
+            A generator of all args from each `refs`s args.
+        """
+        for ref in refs:
+            for arg in ref.args:
+                yield arg
+
+    @staticmethod
+    def _kwargs_get(refs):
+        """Aggregate the iterable `refs.kwargs` into a single .
+
+        Args:
+            refs (Iterable): A list of dicts of kwargs which get aggregated
+                             into a key-value pair iterable.
+
+        Returns:
+            A generator key-value pairs from each `refs`s kwargs.
+        """
+        for ref in refs:
+            for key, value in ref.kwargs.items():
+                yield key, value
+
+    @staticmethod
     def _table(refs):
-        table = refs.next()
-        result = 'FROM {}'.format(table.table)
+        table = next(refs)
+        result = '{}'.format(table.table)
         return result
 
     @staticmethod
     def _select(refs):
-        selects = []
-        for ref in refs:
-            selects.extend(ref.args)
-        result = 'SELECT {}'.format(', '.join(selects))
+        selects = StaticQuery._args_get(refs)
+        result = 'SELECT {} FROM'.format(', '.join(selects))
         return result
 
     @staticmethod
     def _filter(refs):
-        filters = {}
-        for ref in refs:
-            filters.update(ref.kwargs)
-
+        filters = StaticQuery._kwargs_get(refs)
         result = 'WHERE {}'.format(
             ' AND '.join(
                 '{}=\'{}\''.format(key, value) for key, value in filters.items()
@@ -45,36 +156,8 @@ class StaticQuery(object):
         )
         return result
 
-
-class Query(StaticQuery):
-    def __init__(self, table=None, backref=None, func=None, args=None, kwargs=None):
-        self.backref = backref
-        self.table = table
-
-        self.args = args
-        self.kwargs = kwargs
-
-        self.func = func
-        if not func:
-            self.func = Query._table
-
-    def select(self, *args):
-        return Query(backref=self, func=Query._select, args=args)
-
-    def filter(self, **kwargs):
-        return Query(backref=self, func=Query._filter, kwargs=kwargs)
-
-    def build(self):
-        backrefs = sorted(Query._backrefs(self), key=Query._order)
-        groups = groupby(backrefs, lambda k: k.func)
-        result = ' '.join(
-            getattr(Query, key.__name__)(section) for key, section in groups
-        )
-        return result + ';'
-
-print(Query('Person')
-    .select('id')
-    .select('name')
-    .filter(name='Bill', age=18)
-    .build()
-)
+    @staticmethod
+    def _order_by(refs):
+        selects = StaticQuery._args_get(refs)
+        result = 'ORDER BY {}'.format(', '.join(selects))
+        return result
